809c623a1a43250d3b012d89133b8314f0c53336
Oskar <ohavsvik@gmail.com>
2020-09-21 09:36:05 +0200

    Cleanup

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<!-- Viewport for responsive web design -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
 		<title>Boozt | Oskar Havsvik</title>
